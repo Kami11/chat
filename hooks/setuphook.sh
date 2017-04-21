e9dfce15efbdcacbdf1b93edccd20aa9120d62a6
BulatSaif <bulat@saifullin.ru>
2017-04-21 15:27:23 +0300

    pre-push hook

--- hooks/setuphook.sh
+++ hooks/setuphook.sh
@@ -2,5 +2,5 @@
 # 
 # Start with script befor first commit
 ln -s -f ../../hooks/pre-commit ../.git/hooks/pre-commit
-ln -s -f ../../hooks/post-push ../.git/hooks/post-push
+ln -s -f ../../hooks/pre-push ../.git/hooks/pre-push
 echo "symbolic link is created"
