d91fc156a827cecead73fe02508d02e59565747a
BulatSaif <bulat@saifullin.ru>
2017-05-01 15:40:01 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-ssh-keyscan -t rsa -H 104.199.24.157 >> ~/.ssh/known_hosts
-ssh -oStrictHostKeyChecking=no -i secret travis@104.199.24.157 << EOF
+PRODUCTION=104.199.24.157
+PREPRODUCTION=104.199.24.157
+
+#deploy to prep
+ssh-keyscan -t rsa -H $PRODUCTION >> ~/.ssh/known_hosts
+ssh -oStrictHostKeyChecking=no -i secret travis@$PRODUCTION << EOF
 
 pwd
 ls -a
-
+hostname
 echo "I'm inside Google. Here you can run chef or deploy"
 
 EOF
