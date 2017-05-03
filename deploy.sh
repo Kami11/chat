ba8b59b6f88d83480d6ffeddc2f74b0d7063d9d5
bulatsaif <Bulat@saifullin>
2017-05-03 20:28:35 +0300

    Add acceptance test

--- deploy.sh
+++ deploy.sh
@@ -39,34 +39,34 @@ echo "done  deploy to preproduction"
 if ./acceptance_test.sh $PREPRODUCTION
 then
 
-	#############################
-	##  deploy to production   ##
-	#############################
-	ssh-keyscan -t rsa -H $PRODUCTION >> ~/.ssh/known_hosts
-	ssh -oStrictHostKeyChecking=no -i secret travis@$PRODUCTION << EOF
-	echo "***** hostname"
-	hostname
+#############################
+##  deploy to production   ##
+#############################
+ssh-keyscan -t rsa -H $PRODUCTION >> ~/.ssh/known_hosts
+ssh -oStrictHostKeyChecking=no -i secret travis@$PRODUCTION << EOF
+echo "***** hostname"
+hostname
 
-	echo "***** sudo rm -r chat/"
-	sudo rm -r chat/
+echo "***** sudo rm -r chat/"
+sudo rm -r chat/
 
-	echo -e "\n***** git clone https://github.com/Kami11/chat.git"
-	git clone https://github.com/Kami11/chat.git
+echo -e "\n***** git clone https://github.com/Kami11/chat.git"
+git clone https://github.com/Kami11/chat.git
 
-	cd chat/
-	pwd
+cd chat/
+pwd
 
-	echo "\n***** sudo -H ./setup.py install"
-	sudo -H  python3 setup.py install
+echo "\n***** sudo -H ./setup.py install"
+sudo -H  python3 setup.py install
 
-	echo "\n***** sudo killall python3"
-	sudo killall python3
-	EOF
+echo "\n***** sudo killall python3"
+sudo killall python3
+EOF
 
-	echo "**** ssh -i  secret -t travis@$PRODUCTION 'sudo ./chat/bin/kisschat -a 0.0.0.0 -p 80' &"
-	ssh -i  secret -t travis@$PRODUCTION ' cd chat/ && sudo kisschat -a 0.0.0.0 -p 80 ' &
-	sleep 3
-	echo "done deploy to production"
+echo "**** ssh -i  secret -t travis@$PRODUCTION 'sudo ./chat/bin/kisschat -a 0.0.0.0 -p 80' &"
+ssh -i  secret -t travis@$PRODUCTION ' cd chat/ && sudo kisschat -a 0.0.0.0 -p 80 ' &
+sleep 3
+echo "done deploy to production"
 
 else 
 	echo 'The acceptance tests is failed, WebSocket did not work, can not to deploy to production' 
